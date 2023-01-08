96d2f6f6c9cb9a21362472110a36b13bdde22867
Thiago Calomino <38413362+ThiagoClmn@users.noreply.github.com>
2023-01-08 01:28:59 -0300

    Update and rename diferenciation.py to diferentiation.py

--- IntegrationDifferentiation/diferentiation.py
+++ IntegrationDifferentiation/diferentiation.py
@@ -3,7 +3,7 @@
 
 # the function you want to differentiate
 funcao = lambda x : x**2
-h = pow(10,-4)
+h = 1e-4
 
 # f'(x) = [ f(x+h)-f(x) ]/ h   +   o(h)
 # f'(x) = [ f(x)-f(x-h) ]/ h
@@ -11,7 +11,10 @@ h = pow(10,-4)
 
 # f"(x) = [ f(x+h) - 2f(x) + f(x-h) ]/  h**2
 
-first_derivative = lambda x : (funcao(x+h)-funcao(x))/ h
-second_derivate = lambda x : (funcao(x+h) - 2*funcao(x) + funcao(x-h))/(h**2)
+def first_derivative(x):
+  return (funcao(x+h)-funcao(x))/ h
+
+def second_derivate(x):
+  return (funcao(x+h) - 2*funcao(x) + funcao(x-h))/(h**2)
 
 print(second_derivate(2))
